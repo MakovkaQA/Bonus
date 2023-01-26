42bb208811615d2e75f514534bcb091af153b638
Anastasiya <makeeva.anastasya89@gmail.com>
2023-01-27 01:13:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        double balance = 100.78;
+        double refill = 2580.40;
+
+        int bonus;
+
+        if (refill > 1000) {bonus = (int) (refill / 100);} else {bonus = 0;};
+
+        double result = balance + refill + bonus;
+
+        System.out.println("Начислено бонусов: " + bonus);
+        System.out.println("Итого на счету: " + (String.format("%(.2f", result)));
+        }
+    }
